4b90b6f74ea32847d1da77b9f1daac697d5e3f5d
Rare Raptor <connorwtuncc@gmail.com>
2022-07-06 15:11:40 -0400

    fixing pt2

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,23 @@
 const sequelize = require('../config/connection');
-const { Player } = require('../models');
+const { User, Project } = require('../models');
 
-
-const playerData = require('./playerData.json');
+const userData = require('./userData.json');
+const projectData = require('./projectData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Player.bulkCreate(playerData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
