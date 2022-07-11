900eead81f6dea78e949b631dc6a56662d424f77
Rare Raptor <connorwtuncc@gmail.com>
2022-07-11 00:46:48 -0400

    A lot of touch up

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const Game = require('./Game');
 const Player = require('./Player');
 
+Player.belongsTo(Game, {
+  foreignKey: 'player_id',
+});
+
+Game.hasMany(Player, {
+  foreignKey: 'player_id',
+  onDelete: 'CASCADE'
+});
+
+
 module.exports = { Game, Player };
